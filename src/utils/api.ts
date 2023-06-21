535aba789ed6250dd2fc522feb34a199263aef00
Shahzaib <shahzaibghumman453@gmail.com>
2023-06-22 04:38:49 +0500

    updated code

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL || "https://voyagers-server.onrender.com", // Replace with your desired base URL
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_SERVER_URL
+      : process.env.REACT_APP_SERVER_LOCALHOST || "https://voyagers-server.onrender.com", // Replace with your desired base URL
 });
 
 api.interceptors.request.use((config) => {
